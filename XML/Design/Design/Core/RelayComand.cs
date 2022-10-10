fd27c2cb248ba4268b2a16faab6706459346e9fa
VladGUDzik <mr2823844@gmail.com>
2022-10-11 00:40:06 +0300

    add

---
+++ XML/Design/Design/Core/RelayComand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Design.Core
+{
+    class RelayComand:ICommand
+    {
+        private Action<object> _execute;
+        private Func<object, bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+            
+        }
+        public RelayComand(Action<object>execute,Func<object,bool>canExecute=null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public RelayComand()
+        {
+        }
+
+        public bool CanExecute(object parametr)
+        {
+            return _canExecute == null || _canExecute(parametr);
+        }
+
+        public void Execute(object paramter) {
+            _execute(paramter);
+        }
+
+    }
+}
